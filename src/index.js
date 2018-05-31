540aaf2eb42694039e6417da88ed9cb21e3daada
Max Goodman <max.goodman@flatironschool.com>
2018-05-31 17:58:41 -0400

    separation of concerns with functions for DOM manipulation

--- src/index.js
+++ src/index.js
@@ -1,52 +1,96 @@
 document.addEventListener("DOMContentLoaded", function(e){
 	const endPoint = 'http://localhost:3000/api/v1/properties'
+	fetchProperty(endPoint);
+	createPostForm();
+	//includeEditForm();
+	//includeDeleteForm();
+})
+
+function createPostForm(){
+	let createForm = document.getElementById('create-form')
 	let form = document.createElement('form')
-	form.method= "PATCH"
-	form.action = "#"
+	form.method = 'POST'
+	form.id = "form"
+	form.action = '#'
 	let name_edit = document.createElement('input')
-	name_edit.placeholder = "Name"
-	name_edit.type = "text"
+	name_edit.placeholder = 'Name'
+	name_edit.id="name"
+	name_edit.type = 'text'
 	let description_edit = document.createElement('input')
-	description_edit.placeholder = "Description"
-	description_edit.type = "text"
+	description_edit.placeholder = 'Description'
+	description_edit.id="description"
+	description_edit.type = 'text'
 	let address_edit = document.createElement('input')
-	address_edit.placeholder = "Address"
-	address_edit.type = "text"
+		address_edit.id="address"
+	address_edit.placeholder = 'Address'
+	address_edit.type = 'text'
 	let phone_number_edit = document.createElement('input')
-	phone_number_edit.placeholder = "Phone Number"
-	phone_number_edit.type = "text"
+	phone_number_edit.placeholder = 'Phone Number'
+	phone_number_edit.id = 'phone_number'
+	phone_number_edit.type = 'text'
 	let website_edit = document.createElement('input')
-	website_edit.placeholder = "Website"
-	website_edit.type = "text"
+	website_edit.placeholder = 'Website'
+	website_edit.id ="website"
+	website_edit.type = 'text'
 	//click 'save' button
-	let button = document.createElement('button')
+	let button = document.getElementById('submit')
+	button.class = "ui button"
 	button.innerText = "Create New Property"
-	document.body.appendChild(form)
+	createForm.appendChild(form)
 	form.appendChild(name_edit)
 	form.appendChild(description_edit)
 	form.appendChild(address_edit)
 	form.appendChild(phone_number_edit)
 	form.appendChild(website_edit)
 	form.appendChild(button)
-
 	form.addEventListener('submit', function(e){
 		e.preventDefault()
-		fetch(`http://localhost:3000/api/v1/properties/`, {
-			method: "POST",
-		headers: {
-		    'Accept': 'application/json',
-		    'Content-Type': 'application/json'
-		  },
-		  body: JSON.stringify({name: name_edit})
-		})
-		.then( (response) => { 
-		   return response.json()
-		})
-		.then(json => console.log(json))
-//clear all input fields to placeholder
-		name_edit.placeholder = "Name"
-		description_edit.placeholder = "Description"
+		
 	})
+}
+function createPatchForm(){
+	let form = document.createElement('form')
+	form.method = 'PATCH'
+	form.id = "create-update-form"
+	form.action = '#'
+	let name_edit = document.createElement('input')
+	name_edit.placeholder = 'Name'
+	name_edit.id="name"
+	name_edit.type = 'text'
+	let description_edit = document.createElement('input')
+	description_edit.placeholder = 'Description'
+	description_edit.id="description"
+	description_edit.type = 'text'
+	let address_edit = document.createElement('input')
+		address_edit.id="address"
 
-	Property.fetchProperty(endPoint)
-})
+	address_edit.placeholder = 'Address'
+	address_edit.type = 'text'
+	let phone_number_edit = document.createElement('input')
+	phone_number_edit.placeholder = 'Phone Number'
+	phone_number_edit.id = 'phone_number'
+	phone_number_edit.type = 'text'
+	let website_edit = document.createElement('input')
+	website_edit.placeholder = 'Website'
+	website_edit.id ="website"
+	website_edit.type = 'text'
+	//click 'save' button
+	let button = document.createElement('button')
+	button.class = "ui button"
+	button.innerText = 'Create New Property'	
+	prop.appendChild(form)
+	form.appendChild(name_edit)
+	form.appendChild(description_edit)
+	form.appendChild(address_edit)
+	form.appendChild(phone_number_edit)
+	form.appendChild(website_edit)
+	form.appendChild(button)
+}
+function fetchProperty(endpoint){
+		fetch(endpoint)
+		.then(res => res.json())
+		.then(json => json.forEach(property =>{
+			let prop = new Property(property.name, property.description, property.address, property.phone_number, property.website)
+			prop.createProperty();
+	}))
+}
--- src/property.js
+++ src/property.js
@@ -10,138 +10,73 @@ class Property {
 		this.id = ++propertyId
 		Property.all.push(this)
 	}
-
-	static fetchProperty(endpoint){
-		fetch(endpoint)
-		.then(res => res.json())
-		.then(json => json.forEach(property =>{
-			let prop = new Property(property.name, property.description, property.address, property.phone_number, property.website)
+	
+	createProperty(){
+		let properties = document.getElementById('properties')
+		properties.class = "ui cards"
+		let property = document.createElement('div')
+		property.class ="card"
+		property.id = "property"
+			let id = document.createElement('h2')
+			id.innerText = this.id
+			id.class = "header"
 			let name = document.createElement('p')
-			name.innerText = prop.name
+			name.innerText = this.name
 			name.id = "name"
 			let description = document.createElement('p')
-			description.innerText = prop.description
+			description.innerText = this.description
 			description.id ="description"
+			description.class= "description"
 			let address = document.createElement('p')
-			address.innerText = prop.address
+			address.innerText = this.address
 			address.id = "address"
 			let phone_number = document.createElement('p')
-			phone_number.innerText = prop.phone_number
+			phone_number.innerText = this.phone_number
 			phone_number.id = "phone_number"
 			let website = document.createElement('p')
-			website.innerText = prop.website
+			website.innerText = this.website
 			website.id = "website"
 			let hr = document.createElement('hr')
 			let editProp = document.createElement('a')
 			editProp.href = "#"
 			editProp.id = "edit"
 			editProp.innerText = "Edit this Property"
-			editProp.addEventListener('click', function(e){
-				//add form, action ="#" and method 'post'
-				let form = document.createElement('form')
-						form.method= "PATCH"
-						form.action = "#"
-						let name_edit = document.createElement('input')
-						name_edit.placeholder = "Name"
-						name_edit.type = "text"
-						let description_edit = document.createElement('input')
-						description_edit.placeholder = "Description"
-						description_edit.type = "text"
-						let address_edit = document.createElement('input')
-						address_edit.placeholder = "Address"
-						address_edit.type = "text"
-						let phone_number_edit = document.createElement('input')
-						phone_number_edit.placeholder = "Phone Number"
-						phone_number_edit.type = "text"
-						let website_edit = document.createElement('input')
-						website_edit.placeholder = "Website"
-						website_edit.type = "text"
-						//click 'save' button
-						let button = document.createElement('button')
-						button.innerText = "Save"
-						document.body.appendChild(form)
-						form.appendChild(name_edit)
-						form.appendChild(description_edit)
-						form.appendChild(address_edit)
-						form.appendChild(phone_number_edit)
-						form.appendChild(website_edit)
-						form.appendChild(button)
-				//add eventlistener on form
-				form.addEventListener('submit', function(e){
-					e.preventDefault()
-							name_edit.innerText = name_edit.value
-							console.log(name_edit.innerText)
-							description_edit.innerText = description_edit.value
-							address_edit.innerText = address_edit.value
-							website_edit.innerText = website_edit.value
-							phone_number_edit.innerText = phone_number_edit.value
-						//convert them back to title and description after input is done by fetch PATCH
-							fetch(`http://localhost:3000/api/v1/properties/${prop.id}`, {
-								  method: "PATCH",
-								  headers: {
-								    'Accept': 'application/json',
-								    'Content-Type': 'application/json'
-								  },
-
-								  //make sure to serialize your JSON body
-								  body: JSON.stringify({name: name_edit.innerText, description: description_edit.innerText, address: address_edit.innerText, phone_number: phone_number_edit.innerText, website: website_edit.innerText})
-								})
-								.then( (response) => { 
-								   return response.json()
-								})
-								.then(json => console.log(json))
-						})
-					})
-			
 			let br = document.createElement("br")
 			let removeProp = document.createElement('a')
 			removeProp.href = "#"
 			removeProp.id = "remove"
 			removeProp.innerText = "Delete This Property"
-			removeProp.addEventListener('click', function(e){
-				//remove elements from page
-					document.body.removeChild(name)
-				document.body.removeChild(description)
-				document.body.removeChild(address)
-				document.body.removeChild(phone_number)
-				document.body.removeChild(website)
-				document.body.removeChild(editProp)
-				document.body.removeChild(br)
-				document.body.removeChild(removeProp)
-				//delete post by id
-				fetch(`http://localhost:3000/api/v1/properties/${prop.id}`, {
-					method: "DELETE",
-				  headers: {
-				    'Accept': 'application/json',
-				    'Content-Type': 'application/json'
-				  },
+			properties.appendChild(property)
+			property.appendChild(id)
+			property.appendChild(name)
+			property.appendChild(description)
+			property.appendChild(address)
+			property.appendChild(phone_number)
+			property.appendChild(website)
+			property.appendChild(editProp)
+			property.appendChild(br)
+			property.appendChild(removeProp)
+	}
 
-				  //make sure to serialize your JSON body
-				  body: JSON.stringify({
-				    name: prop.name,
-				    description: prop.description, 
-				    address: prop.address, 
-				    phone_number: prop.phone_number, 
-				    website: prop.website
-				  })
-				})
-				.then(response => response.json())
-				.then(json => console.log(json))
-			})
-			document.body.appendChild(name)
-			document.body.appendChild(description)
-			document.body.appendChild(address)
-			document.body.appendChild(phone_number)
-			document.body.appendChild(website)
-			document.body.appendChild(editProp)
-			document.body.appendChild(br)
-			document.body.appendChild(removeProp)
-			// document.body.appendChild(hr)
-			// editProp.addEventListener()
-			// removeProp.addEventListener('click', function(e){
+	deleteProperty(){
+		fetch(`http://localhost:3000/api/v1/properties/${property.id}`, {
+			method:"DELETE", 
+			headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
 
-			// })
-		}))
+  //make sure to serialize your JSON body
+  body: JSON.stringify({
+    name: this.name,
+    description: this.description, 
+    address: this.address, 
+    phone_number: this.phone_number, 
+    website: this.website
+  })
+		})
+		.then(response => response.json())
+		.then(json => console.log(json))
 	}
 }
 
